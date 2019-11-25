93d7b972a5e09d7ffce4fe39f3a467c5e3c4b554
geekwise-Jesus-Garcia <jesus1287garcia@yahoo.com>
2019-11-25 18:12:07 +0000

    created a rest api test with quickstart tutor

--- app/drf/views.py
+++ app/drf/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializer import User_Serializer, Group_Serializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = User_Serializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = Group_Serializer
